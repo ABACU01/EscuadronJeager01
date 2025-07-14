4cb02c0094397f9a6c78e7647997d50759aa9dd6
ABACU01 <141466578+ABACU01@users.noreply.github.com>
2025-07-14 17:47:56 -0600

    Update wsgi.py

--- EscuadronJeager01/wsgi.py
+++ EscuadronJeager01/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EscuadronJeager01.settings')
+
+# Configuramos Django
+django.setup()
+
+# Ejecutamos las migraciones automáticamente al iniciar la app
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    # Aquí puedes poner un print para ver errores, o manejar el error de otra forma
+    print(f"Error al correr migraciones automáticamente: {e}")
 
 application = get_wsgi_application()
